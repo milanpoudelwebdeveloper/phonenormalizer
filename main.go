6bcd4f2dea763fc3d0ef7574ca71b31871c8e7d1
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-06-08 18:25:01 +0545

    Use of bytes buffer to scan only digits

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
+import "bytes"
+
 type Person struct {
 	Name  string
 	Phone string
 }
 
 func normalize(phone string) string {
-	return ""
+	//will be good for processing large sets of data
+	var buf bytes.Buffer
+	for _, ch := range phone {
+		//just scan through the phone number and if it is a digit, write it to the buffer
+		if ch >= '0' && ch <= '9' {
+			buf.WriteRune(ch)
+		}
+	}
+	//gives us the string representation of the buffer written to buf
+	return buf.String()
+	//we want real digits only  (123) 456-7893 -> 1234567893
+
 }
